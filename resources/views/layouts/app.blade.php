332a89e24a330424bd9d02077786afea74034344
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-10-23 00:10:38 +0200

    Struttura base

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('page-title') | Laravel Start 1</title>
+        <title>laravel-model-controller</title>
 
         {{-- Stili nell'head --}}
         <link rel="stylesheet" href="css/style.css">
@@ -17,32 +17,18 @@
     <body>
 
         @include('partials.header')
+        <div>
 
-        <main>
-            <div class="container">
+        </div>
+        
+        @yield('main-content')
+        <div>
 
-                <div class="row">
-
-                    <div class="col">
-
-                        <div>
-                            <img class="gatto-img" src="{{ Vite::asset('resources/img/gatto.webp') }}" alt="Gatto">
-                        </div>
-
-                        <div class="gatto-bg">
-                            QUESTO DIV AVRA' COME SFONDO IL GATTO
-                        </div>
-
-                        @yield('main-content')
-
-                    </div>
-
-                </div>
-
-            </div>
-        </main>
-
+        </div>
         @include('partials.footer')
+        <div>
+
+        </div>
 
         {{-- JS da importare nel body --}}
         @yield('body-imports')
