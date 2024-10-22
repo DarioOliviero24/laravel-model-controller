beb02a32380fa6eac4c175c13f250ad2e4a18a2d
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-10-23 01:42:02 +0200

    Stampati in pagine con una tabe id e title film , usato il controller

---
+++ app/Http/Controllers/Guest/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Models
+use App\Models\Movie;
+
+
+class MainController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+
+        return view('welcome', [
+            'movies' => $movies
+        ]);
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,28 @@
 @section('page-title', 'Home')
 
 @section('main-content')
+<h1 class="text-center py-5">
+    Table Laravel Controller
+</h1>
+
+<table class="table container">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Title</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($movies as $movie)
+        <tr>
+            <th scope="row">{{ $movie->id }}</th>
+            <td colspan="2">{{ $movie->title }}</td>
+            <td></td>
+        </tr>
+    @endforeach
+    
+  </tbody>
+</table>
 
 @endsection
